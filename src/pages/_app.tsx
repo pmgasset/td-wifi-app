cb7be90f0bb7407105a0cc5f4271bbff08c2570f
pmgasset <david@pmgasset.com>
2025-08-06 11:13:06 -0400

    feat: add Auth0 protection to member page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 // src/pages/_app.tsx
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/next'
+import { UserProvider } from '@auth0/nextjs-auth0/client'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <UserProvider>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </UserProvider>
   )
 }
